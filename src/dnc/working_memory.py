10c15f135c38d5c712a06ed9cc0607d28f4c5e84
ixaxaar <root@ixaxaar.in>
2017-09-02 20:33:38 +0530

    Pass around hidden states

--- src/dnc/working_memory.py
+++ src/dnc/working_memory.py
@@ -20,24 +20,39 @@ class WorkingMemory(nn.Module):
     self.read_heads = read_heads
     self.gpu_id = gpu_id
 
-  def reset(self, batch_size=1):
+  def reset(self, batch_size=1, hidden=None):
     m = self.mem_size
     w = self.cell_size
     r = self.read_heads
     b = batch_size
 
-    # linear memory (b * m * w)
-    self.memory = cuda(T.zeros(b, m, w).fill_(δ), gpu_id=self.gpu_id)
-    # associative linkages (b * m * m)
-    self.temporal = cuda(T.zeros(b, m, m), gpu_id=self.gpu_id)
-    # self.semantic = cuda(T.zeros(b, m, m), gpu_id=self.gpu_id)
-    self.temporal_weights = cuda(T.zeros(b, m), gpu_id=self.gpu_id)
-    # self.semantic_weights = cuda(T.zeros(b, m), gpu_id=self.gpu_id)
-    self.read_weights = cuda(T.zeros(b, m, r).fill_(δ), gpu_id=self.gpu_id)
-    self.write_weights = cuda(T.zeros(b, m).fill_(δ), gpu_id=self.gpu_id)
-    # self.read_vector = cuda(T.zeros(b, w, r).fill_(δ), self.gpu_id=self.gpu_id)
-    self.usage_vector = cuda(T.zeros(b, m), gpu_id=self.gpu_id)
-    self.I = cuda(T.eye(m).unsqueeze(0))  # (1 * n * n)
+    self.I = cuda(1 - T.eye(m).unsqueeze(0), gpu_id=self.gpu_id)  # (1 * n * n)
+
+    if hidden is None:
+      return {
+          # linear memory (b * m * w)
+          'memory': cuda(T.zeros(b, m, w).fill_(δ), gpu_id=self.gpu_id),
+          # associative linkages (b * m * m)
+          'temporal': cuda(T.zeros(b, m, m), gpu_id=self.gpu_id),
+          # 'semantic' : cuda(T.zeros(b, m, m), gpu_id=self.gpu_id)
+          'temporal_weights': cuda(T.zeros(b, m), gpu_id=self.gpu_id),
+          # 'semantic_weights' : cuda(T.zeros(b, m), gpu_id=self.gpu_id)
+          'read_weights': cuda(T.zeros(b, m, r).fill_(δ), gpu_id=self.gpu_id),
+          'write_weights': cuda(T.zeros(b, m).fill_(δ), gpu_id=self.gpu_id),
+          # 'read_vector' : cuda(T.zeros(b, w, r).fill_(δ), self.gpu_id=self.gpu_id)
+          'usage_vector': cuda(T.zeros(b, m), gpu_id=self.gpu_id)
+      }
+    else:
+      hidden['memory'].data.fill_(δ)
+      hidden['temporal'].data.fill_(δ)
+      # hidden['semantic'].data.fill_(δ)
+      hidden['temporal_weights'].data.fill_(δ)
+      # hidden['semantic_weights'].data.fill_(δ)
+      hidden['read_weights'].data.fill_(δ)
+      hidden['write_weights'].data.fill_(δ)
+      # hidden['read_vector'].data.fill_(δ)
+      hidden['usage_vector'].data.fill_(δ)
+    return hidden
 
   def mem_usage(self, usage, free_gates, read_weights, write_weight):
     ψ = T.prod(1 - free_gates.unsqueeze(1).expand_as(read_weights) * read_weights, 2)
@@ -50,7 +65,7 @@ class WorkingMemory(nn.Module):
     sorted_allocation_weights = (1 - sorted_usage) * T.cumprod(sorted_usage, dim=1)
     # construct the reverse sorting index https://stackoverflow.com/questions/2483696/undo-or-reverse-argsort-python
     φ_rev = cuda(T.zeros(φ.size()), gpu_id=self.gpu_id)
-    x = T.arange(0, φ_rev.size()[1], 1).unsqueeze(0).expand_as(φ)
+    x = cuda(T.arange(0, φ_rev.size()[1], 1).unsqueeze(0).expand_as(φ), gpu_id=self.gpu_id)
     φ_rev.scatter_(1, φ, x)
 
     allocation_weights = sorted_allocation_weights.gather(1, φ_rev.long())
@@ -72,35 +87,34 @@ class WorkingMemory(nn.Module):
 
     temporal = (prev_scale * temporal + new_temporal).squeeze(4).squeeze(1)
     # elaborate trick to delete diag elems
-    temporal = (1 - self.I).expand_as(temporal) * temporal
-    return temporal
+    return self.I.expand_as(temporal) * temporal
 
   def update_precedence(self, precedence, write_weight):
     return (1 - T.sum(write_weight, 1)).expand_as(precedence) * precedence + write_weight
 
-  def write(self, write_key, write_vector, erase_vector, free_gates, read_strengths, write_strength, write_gate, allocation_gate):
+  def write(self, write_key, write_vector, erase_vector, free_gates, read_strengths, write_strength, write_gate, allocation_gate, hidden):
     # lookup memeory with write_key
-    φ = θ(self.memory, write_key, dimB=2).squeeze(1)
+    φ = θ(hidden['memory'], write_key, dimB=2).squeeze(1)
     # get current usage
-    self.usage_vector = self.mem_usage(self.usage_vector, free_gates, self.read_weights, write_strength)
+    hidden['usage_vector'] = self.mem_usage(hidden['usage_vector'], free_gates, hidden['read_weights'], write_strength)
     # get memory allocation
-    alloc = self.allocation(self.usage_vector)
+    alloc = self.allocation(hidden['usage_vector'])
     # get write weightings
-    self.write_weights = \
-        self.write_weighting(self.memory, φ, alloc, write_gate, allocation_gate)
+    hidden['write_weights'] = \
+        self.write_weighting(hidden['memory'], φ, alloc, write_gate, allocation_gate)
 
-    write_weights = self.write_weights.unsqueeze(2)
+    write_weights = hidden['write_weights'].unsqueeze(2)
     write_vector = write_vector.unsqueeze(1)
     erase_vector = erase_vector.unsqueeze(1)
 
     # Update memory
-    erasing = self.memory * (1 - T.bmm(write_weights, erase_vector))
-    writing = T.bmm(write_weights, write_vector)
-    self.memory = erasing + writing
+    hidden['memory'] = hidden['memory'] * (1 - T.bmm(write_weights, erase_vector))
+    hidden['memory'] = hidden['memory'] + T.bmm(write_weights, write_vector)
 
     # update link_matrix
-    self.temporal = self.get_link_matrix(self.temporal, write_weights, self.temporal_weights)
-    self.temporal_weights = self.update_precedence(self.temporal_weights, write_weights)
+    hidden['temporal'] = self.get_link_matrix(hidden['temporal'], write_weights, hidden['temporal_weights'])
+    hidden['temporal_weights'] = self.update_precedence(hidden['temporal_weights'], write_weights)
+    return hidden
 
   def content_weightings(self, memory, keys, read_strengths):
     d = θ(memory, keys, dimB=2)  # b * m * r
@@ -125,19 +139,19 @@ class WorkingMemory(nn.Module):
   def read_vectors(self, memory, read_weights):
     return T.bmm(memory.transpose(1, 2), read_weights)
 
-  def read(self, read_keys, read_strengths, read_modes):
-    self.read_weights = self.read_weightings(self.memory, read_keys, self.read_weights, self.temporal, read_modes)
-    read_vectors = self.read_vectors(self.memory, self.read_weights)
-    return read_vectors
+  def read(self, read_keys, read_strengths, read_modes, hidden):
+    hidden['read_weights'] = self.read_weightings(
+        hidden['memory'], read_keys, hidden['read_weights'], hidden['temporal'], read_modes
+    )
+    read_vectors = self.read_vectors(hidden['memory'], hidden['read_weights'])
+    return read_vectors, hidden
 
-  def forward(self, ξ):
+  def forward(self, ξ, hidden):
 
     m = self.mem_size
     w = self.cell_size
     r = self.read_heads
     b = ξ.size()[0]
-
-    self.reset(b)
 
     # r read keys (b * r * w)
     read_keys = ξ[:, :r * w].contiguous().view(b, w, r)
@@ -160,6 +174,6 @@ class WorkingMemory(nn.Module):
     # read modes (b * 3*r)
     read_modes = σ(ξ[:, r * w + 2 * r + 3 * w + 2: r * w + 5 * r + 3 * w + 2].contiguous().view(b, 3, r), 1)
 
-    self.write(write_key, write_vector, erase_vector, free_gates,
-               read_strengths, write_strength, write_gate, allocation_gate)
-    return self.read(read_keys, read_strengths, read_modes)
+    hidden = self.write(write_key, write_vector, erase_vector, free_gates,
+                        read_strengths, write_strength, write_gate, allocation_gate, hidden)
+    return self.read(read_keys, read_strengths, read_modes, hidden)
